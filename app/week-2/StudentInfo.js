31ef14c5fb3ebc0d6bb97b988b72b33012f23944
helaniedom <helanied18@gmail.com>
2026-01-23 13:25:38 -0700

    week 2 root page, student info finished

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <p>Helanie Dominguez</p>
+            <Link href="https://github.com/helaniedom/cprg306-assignments">https://github.com/helaniedom/cprg306-assignments
+            </Link>
+        </div>
+    );
+}
+ 

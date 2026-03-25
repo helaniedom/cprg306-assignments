2766a9142c6f5caa8ab2a53a4229f8ec21f9fde3
helaniedom <helanied18@gmail.com>
2026-03-25 11:56:23 -0600

    added the getitems and additems function

--- app/week-10/_services/shopping-list-service.js
+++ app/week-10/_services/shopping-list-service.js
@@ -1,2 +1,21 @@
 import { db } from "../utils/firebase";
 import { collection, getDocs, addDoc, query } from "firebase/firestore";
+
+export async function getItems(userId) {
+    const items = [];
+    const itemsCollection = collection(db, "users", userId, "items");
+    const q = query(itemsCollection);
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach((doc) => {
+        items.push({ id: doc.id, ...doc.data() });
+    });
+
+    return items;
+}
+
+export async function addItem(userId, item) {
+    const itemsCollection = collection(db, "users", userId, "items");
+    const docRef = await addDoc(itemsCollection, item);
+    return docRef.id;
+}
